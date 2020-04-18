8949005441a1e7ddd7b7125f1f10ce7397be2885
PedroRib76 <p.alessandro@uol.com.br>
2020-04-17 23:41:19 -0300

    Codigos complementados na sistema

---
+++ Secao9/Entities/Department.cs
@@ -0,0 +1,17 @@
+﻿namespace Secao9.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ Secao9/Entities/Worker.cs
@@ -0,0 +1,27 @@
+﻿using Secao9.Entities.Enums;
+using System.Collections.Generic;
+
+namespace Secao9.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            Level = level;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+    }
+}
